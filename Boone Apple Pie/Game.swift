c3970d5ad8466f129f290e3419dc3b612286a31e
D'Kairi Boone <d.boone@setonhill.edu>
2023-03-26 12:23:36 -0400

    Ready

---
+++ Boone Apple Pie/Game.swift
@@ -0,0 +1,40 @@
+//
+//  Game.swift
+//  Boone Apple Pie
+//
+//  Created by d.boone on 3/26/23.
+//
+
+import Foundation
+
+struct Game {
+    var word : String
+    var incorrectMovesRemaining : Int
+    var guessedLetters: [Character]
+    
+    mutating func playerGuessed(letter: Character){
+        guessedLetters.append(letter)
+        if !word.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+        
+        
+
+        
+    }
+    
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
+
+    
+}
+
